714e42a3ad8e0c3c6a1509c63d4acea2e2a92983
Rodion Mern <114694164+rodionmern@users.noreply.github.com>
2023-11-24 21:17:46 +0300

    Script update and UI

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,36 @@
 var cartName, priceName;
 
 function addToCart() {
-	var priceDOM = document.getElementById('price').innerHTML;
-	var nameDOM = document.getElementById('nameProd').innerHTML;
+	var priceDOM = document.getElementById('product_price_one').innerHTML;
+	var nameDOM = document.getElementById('product_name_one').innerHTML;
 	console.log(priceDOM,nameDOM)
 	localStorage.setItem('price', priceDOM)
 	localStorage.setItem('name', nameDOM)
-	var cartName = document.getElementById('cartName').innerHTML = localStorage.getItem('name');
-	var priceName = document.getElementById('priceName').innerHTML = localStorage.getItem('price');
+	// var cartName = document.getElementById('cartName').innerHTML = localStorage.getItem('name');
+	// var priceName = document.getElementById('priceName').innerHTML = localStorage.getItem('price');
+
+	// var newDiv = document.createElement("div");
+	// newDiv.innerHTML = `
+	// 					<div class="cart__description">
+	// 						<div class="produuct">
+	// 							<p id="${nameDOM}">Неиз3231вестный товар</p>
+	// 							<p id="${priceDOM}">223???₽</p>
+	// 						</div>
+	// 					</div>`;
+
+	var parent = document.querySelector('.cart');
+
+	var cart__ = document.createElement('div');
+	parent.appendChild(cart__);
+
+	var name = document.createElement('p');
+	name.textContent = `${nameDOM}`;
+
+	var price = document.createElement('p');
+	price.textContent = `${priceDOM}`;
+
+	cart__.appendChild(name);
+	cart__.appendChild(price);
 }
 
 function clearCart() {
